d9bdfe1677693474f170fdc714299f4d04da4cc5
shubham79a <shubh79.nolan@gmail.com>
2025-11-10 22:33:37 +0530

    Codeforces Contest Round 1062 (DIV 4) LIVE

---
+++ codeforces/Round 1063 (Div. 2) LIVE/problem_01.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n+1);   
+        for(int i=1;i<=n;i++) cin>>a[i];
+        string x;
+        cin>>x;
+
+        
+           
+    }
+}
